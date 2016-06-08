bc68eb91bceb2ebe85ac8be5c93e0bd6b881e5c4
rikki0405 <rikki-jones@hotmail.com>
2016-06-08 22:47:54 +0900

    Create PE_001.cpp

---
+++ PE_001.cpp
@@ -0,0 +1,41 @@
+/*Project Euler #1: Multiples of 3 and 5  */
+
+#include <iostream>
+#include <iomanip>
+
+int main() {
+    
+    /* Number of test cases */
+    int t = 0;
+    std::cin >> t;
+    std::cin.ignore();
+    
+    for (int i = 0; i < t; i++) {
+        
+        long sum = 0.0;   // long, to work with large numbers
+        long n = 0;
+        std::cin >> n;
+        std::cin.ignore();
+        
+        /* n / 2 (2a + (n - 1) d) <- summation formula */
+        
+        /* Add the numbers divisible by 3 */
+        long double max = (n - 1) / 3;
+        sum = sum + ((max / 2) * ( (2 * 3) + ((max - 1) * 3) ) );
+        
+        /* Add the numbers divisible by 5 */
+        max = (n - 1) / 5;
+        sum = sum + ((max / 2) * ( (2 * 5) + ((max - 1) * 5) ) );
+        
+        /* Minus the numbers divisible by 15 (remove doubles) */
+        if (n > 15) {
+            max = (n - 1) / 15;
+            sum = sum - ((max / 2) * ( (2 * 15) + ((max - 1) * 15) ) );
+        }
+        
+        std::cout << sum << std::endl;
+    }
+    
+    return 0;
+}
+
