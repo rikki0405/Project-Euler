f7f1440251d918316b64da60b35b3a95e9fe9bae
rikki0405 <rikki-jones@hotmail.com>
2016-06-24 11:29:08 +0900

    Project Euler 003: Largest Prime Factor
    Complete rewrite of the code. 
    Current runtime is vastly superior to prior code.

--- PE_003.cpp
+++ PE_003.cpp
@@ -1,43 +1,35 @@
-/* Project Euler 003: Largest Prime Factor - HackerRank Modified - Yesh_02 Approach */
+/* Project Euler 003: Largest Prime Factor - HackerRank Modified */
 #include <iostream>
-#include <cmath>
+#include <cmath>        // sqrt()
 
-long factorize(const long &n) {
+static unsigned long factor(const unsigned long &n) {
+    unsigned long _n = n;
+    while (_n % 2 == 0) _n /= 2;
     
-    /* Step 1: if the given N is even, repeatedly divide by 2 */
-    long factor_n = n;
-    while (factor_n % 2 == 0) { factor_n = factor_n / 2; }
+    if (_n == 1) return 2;
     
-    /* Step 2: if N = 1, return 2 */
-    if (factor_n == 1) { return 2; }
-    
-    /* Step 3: start with i=3, and check if the N is divisible by i. Increment i by 2. Run loop till sqrt of N */
-    long i = 3;
-    while (i != sqrt(factor_n)) {
-        if (n % i == 0) {
-            factor_n = n;
-            break;
-        } else {
-            i = i + 2;
+    unsigned long i;
+    for (i = 3; i <= sqrt(_n); i += 2) {
+        if (_n % i == 0) {
+            _n /= i;
+            i = 1;
         }
     }
-    
-    /* Step 3: if N > 2, return N. Else return i */
-    if (factor_n > 2) { return factor_n; }
-    return i;
+    if (_n > 2) return _n;
+    return _n - 2;
 }
 
 int main() {
     
-    int n;
-    std::cin >> n;
+    int t;
+    std::cin >> t;
     std::cin.ignore();
     
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < t; i++) {
         long check;
         std::cin >> check;
         std::cin.ignore();
-        std::cout << factorize(n) << std::endl;
+        std::cout << factor(check) << std::endl;
     }
     
     return 0;
